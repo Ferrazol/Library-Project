6c3a281cf17980fb2c455a9b4bc452ac15046c13
Ferrazol <huf@alunos.utfpr.edu.br>
2020-12-17 15:45:13 -0300

    oi

--- book.js
+++ book.js
@@ -23,29 +23,80 @@ function addBookToLibrary() {
   render();
 }
 
-function render(){
-    const selecionartd = document.getElementById('livros');
-    while (selecionartd.firstChild){
-        selecionartd.removeChild(selecionartd.firstChild);
-    }
-    for (let i=0; i<myLibrary.length; i++){
-        criarLivro(myLibrary[i]);
-    }
+function render() {
+  // pega o elemento de Id livros
+  const getLivros = document.getElementById("livros");
+  //enquanto tiver um elemento DOM filho, remove ele
+  while (getLivros.firstChild) {
+    getLivros.removeChild(getLivros.firstChild);
+  }
+  //recriar os elementos filhos dentro do Id 'livros'
+  for (let i = 0; i < myLibrary.length; i++) {
+    criarLivro(myLibrary[i]);
+  }
 }
 
-function criarLivro(item) {
-    //pega a minha div chamada livros
-    let list = document.getElementById("livros");
-    console.log(item);
-    const card = `<div class="card">
-    <div class="container">
-      <h4><b> ${item.title}</b></h4>
-      <p>Autor: ${item.author}</p>
-      <p>Pages: ${item.pages}</p>
-      <p>Read: ${item.read}</p>
-    </div>
-  </div>`
-    const ele = document.createElement("div");
-    ele.innerHTML = card;
-    list.appendChild(ele);
+function criarLivro(livroInfo) {
+  // *** Cria um card livro, recebendo de parâmetro o array
+  // myLibrary que contém objetos com info do livro
+  //pega a div
+  let n = myLibrary.indexOf(livroInfo);
+  console.log(n);
+  const list = document.getElementById("livros");
+  //cria um card contendo as info do livro
+  //**************** */
+  let card = document.createElement("div");
+  card.setAttribute("class", "card");
+
+  let title = document.createElement("h1");
+  title.innerHTML = livroInfo.title;
+
+  let author = document.createElement("p");
+  author.innerHTML = "Author: " + livroInfo.author;
+
+  let pages = document.createElement("p");
+  pages.innerHTML = "Pages: " + livroInfo.pages;
+
+  let read = document.createElement("p");
+  if (livroInfo.read ===false){
+    read.innerHTML = 'Not Read';
+    read.style.backgroundColor = 'red';
+  }
+  else {
+    read.innerHTML = 'Read';
+    read.style.backgroundColor = 'green';
+  }
+
+  // "Read: " + livroInfo.read;
+  read.setAttribute('id', 'read'+ n)
+
+  let rmvbutton = document.createElement("button");
+  rmvbutton.innerHTML = "Remove";
+  rmvbutton.setAttribute("class", "novoLivro");
+  rmvbutton.setAttribute('id', 'rmvButton'+ n)
+
+  let readbutton = document.createElement("button");
+  readbutton.innerHTML = "Read";
+  readbutton.setAttribute("class", "novoLivro");
+  readbutton.setAttribute('id', 'readButton'+ n)
+  //*************** */
+  //ADICIONA OS OBJETOS CRIADOS DENTRO DO CARD CRIADO
+  card.appendChild(title);
+  card.appendChild(author);
+  card.appendChild(pages);
+  card.appendChild(read);
+  card.appendChild(rmvbutton);
+  card.appendChild(readbutton);
+  //COLOCA O CARD DENTRO DA DIV NA PAGINA PRINCIPAL
+  list.appendChild(card);
+
+  // COLOCA UMA FUNÇÃO DE REMOVER PARA O BOTAO COM ID EM QUESTÃO
+  document.getElementById('rmvButton'+n).addEventListener("click", () => {
+    myLibrary.splice(n, 1);
+    render();
+  });
+  document.getElementById('readButton'+n).addEventListener("click", () => {
+    myLibrary[n].read = true;
+    render();
+  });
 }
